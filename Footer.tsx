c5f2f5886b8014048e24f13c290588009bce4961
hamdi-HTR <hamdikaryanto@gmail.com>
2026-02-07 09:46:02 +0700

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center py-6 mt-10">
-      <p className="text-sm">
-        © {new Date().getFullYear()} Jurusan TKJ - SMKS Pustek Mitra Tigaraksa
-      </p>
-    </footer>
-  );
-}
